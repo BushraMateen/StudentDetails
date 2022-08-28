7435419110aa2f141603211cd9fd6b7c63f33436
BushraMateen <bushra.mateen95@outlook.com>
2022-08-28 19:10:31 +0530

    implemented put method

--- StudentDetails/Controllers/StudentsController.cs
+++ StudentDetails/Controllers/StudentsController.cs
@@ -12,7 +12,7 @@ namespace StudentDetails.Controllers
     {
         public IEnumerable<SchoolData> Get()
         {
-            using(STMSEntities entities = new STMSEntities())
+            using (STMSEntities entities = new STMSEntities())
             {
                 return entities.SchoolDatas.ToList();
             }
@@ -22,14 +22,14 @@ namespace StudentDetails.Controllers
             using (STMSEntities entities = new STMSEntities())
             {
                 var entity = entities.SchoolDatas.FirstOrDefault(e => e.id == id);
-                if(entity != null)
+                if (entity != null)
                 {
                     return Request.CreateResponse(HttpStatusCode.OK, entity);
 
                 }
                 else
                 {
-                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id " + id.ToString()  +  "not found");
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id " + id.ToString() + "not found");
                 }
             }
         }
@@ -48,11 +48,11 @@ namespace StudentDetails.Controllers
                     return message;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
-            
+
         }
         public HttpResponseMessage Delete(int id)
         {
@@ -63,14 +63,14 @@ namespace StudentDetails.Controllers
                     var entity = entities.SchoolDatas.FirstOrDefault(e => e.id == id);
                     if (entity == null)
                     {
-                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id " +  id.ToString() + "not found to delete");
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id " + id.ToString() + "not found to delete");
 
                     }
                     else
                     {
                         entities.SchoolDatas.Remove(entity);
                         entities.SaveChanges();
-                        return Request.CreateResponse(HttpStatusCode.OK,"Deleted succesfully");
+                        return Request.CreateResponse(HttpStatusCode.OK, "Deleted succesfully");
 
                     }
                 }
@@ -81,6 +81,37 @@ namespace StudentDetails.Controllers
 
             }
         }
-    
-    }
-    }
+        public HttpResponseMessage Put(int id, [FromBody] SchoolData schooldata)
+        {
+            try
+            {
+                using (STMSEntities entities = new STMSEntities())
+                {
+                    var entity = entities.SchoolDatas.FirstOrDefault(e => e.id == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id = " + id.ToString() + "not found to update");
+
+                    }
+                    else
+                    {
+                        entity.SchoolCode = schooldata.SchoolCode;
+                        entity.SchoolName = schooldata.SchoolName;
+                        entity.District = schooldata.District;
+                        entity.Block = schooldata.Block;
+                        entity.Cluster = schooldata.Cluster;
+                        entity.Status = schooldata.Status;
+                        entity.SchoolCode = schooldata.SchoolCode;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity, "updated succesfully");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+    }   
+    } 
