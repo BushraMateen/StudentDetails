9271b51c178821af8f465fe1424dbf4835797977
BushraMateen <bushra.mateen95@outlook.com>
2022-08-28 15:33:35 +0530

    written post api

--- StudentDetails/App_Start/WebApiConfig.cs
+++ StudentDetails/App_Start/WebApiConfig.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 
 namespace StudentDetails
 {
+    //public class CustomJsonFormatter : JsonMediaTypeFormatter
+    //{
+    //    public CustomJsonFormatter()
+    //    {
+    //        this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+    //    }
+
+    //    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+    //    {
+    //        base.SetDefaultContentHeaders(type, headers, mediaType);
+    //        headers.ContentType = new MediaTypeHeaderValue("application/json");
+    //    }
+
+    //}
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -19,6 +35,10 @@ namespace StudentDetails
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+           // config.Formatters.Add(new CustomJsonFormatter());
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
--- StudentDetails/Controllers/StudentsController.cs
+++ StudentDetails/Controllers/StudentsController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using StudentDataAccess;
 
@@ -13,6 +16,34 @@ namespace StudentDetails.Controllers
             {
                 return entities.SchoolDatas.ToList();
             }
+        }
+        public SchoolData Get(int id)
+        {
+            using (STMSEntities entities = new STMSEntities())
+            {
+                return entities.SchoolDatas.FirstOrDefault(e => e.id == id);
+            }
+        }
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] SchoolData schooldata)
+        {
+            try
+            {
+                using (STMSEntities entities = new STMSEntities())
+                {
+                    entities.SchoolDatas.Add(schooldata);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, schooldata);
+                    message.Headers.Location = new Uri(Request.RequestUri + schooldata.id.ToString());
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
         }
     }
 }
